66f63989ee67f1c3d2220030bd06b3185a4cba95
LaveshJagwani <jagwanilavesh@gmail.com>
2026-02-14 15:39:12 +0530

    Fix

--- database.py
+++ database.py
@@ -4,9 +4,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
+from sqlalchemy import create_engine
+import os
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}  # 🔥 REQUIRED for Render
+)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
